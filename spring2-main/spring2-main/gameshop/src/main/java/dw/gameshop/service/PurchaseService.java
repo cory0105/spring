9338dc88464d28c07e7c20d3063bac6a237fec33
cory0105 <161662652+cory0105@users.noreply.github.com>
2024-05-30 17:31:34 +0900

    0530

--- spring2-main/spring2-main/gameshop/src/main/java/dw/gameshop/service/PurchaseService.java
+++ spring2-main/spring2-main/gameshop/src/main/java/dw/gameshop/service/PurchaseService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -25,6 +26,18 @@ public class PurchaseService {
         //구매확정 바로 직전, 현재시간을 저장함
         purchase.setPurchaseTime(LocalDateTime.now());
         return purchaseRepository.save(purchase);
+    }
+
+    public List<Purchase> savePurchaseList(List<Purchase> purchaseList){
+//        for (int i=0; i<purchaseList.size(); i++){
+//            purchaseList.get(i).setPurchaseTime(LocalDateTime.now());
+//        }
+//        return purchaseRepository.saveAll(purchaseList);
+        List<Purchase> savePurchaseList = purchaseList.stream().map((purchase)->{
+            purchase.setPurchaseTime(LocalDateTime.now());
+            return purchaseRepository.save(purchase);
+        }).collect(Collectors.toList());
+        return savePurchaseList;
     }
 
     public List<Purchase> getAllPurchases() {
---
+++ spring2-main/spring2-main/gameshop/src/main/resources/static/js/cart.js
@@ -0,0 +1,39 @@
+const url = "http://localhost:8080/api/products/purchaselist";
+
+function sessionCurrent(){
+    axios
+    .get("http://localhost:8080/user/current", {withCredentials:true})
+    .then((response)=>{
+        console.log("데이터: ", response.data);
+        if(response.status == 200){
+            const userId = response.data;
+            let cartItems = JSON.parse(localStorage.getItem(userId));
+            if(cartItems){
+                const data = cartItems.map((game)=>{
+                    // Purchase 객체를 만들어서 리턴
+                    return {game:game, user:{userId: userId}};
+                })
+                document.querySelector(".purchaseBtn").addEventListener("click", ()=>{
+                    if(confirm("진짜 구매하시겠습니까?")){
+                        axios
+                        .post(url, data, {withCredentials: true})
+                        .then((response)=>{
+                            console.log("데이터: ", response.data);
+                            localStorage.removeItem(userId);
+                        })
+                        .catch((error)=>{
+                            console.log("에러 발생: ", error);
+                        })
+                    }
+                });                
+            }
+        }
+    })
+    .catch((error)=>{
+        console.log("에러 발생: ", error);
+        alert("로그인해주세요.");
+    })
+};
+
+// 페이지 로딩 시에 즉시 세션여부 확인
+sessionCurrent();
